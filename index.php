574ef181184f2251aec5dd796a8f048d18256e09
lindseyemaddox <lindseyemaddox@gmail.com>
2015-04-11 23:35:34 -0500

    H styles, style header text

--- index.php
+++ index.php
@@ -6,13 +6,37 @@
 	<div class="flexslider carousel">
 
 		<ul class="slides">
+
 			<li>
+
+				<div class="inner">
+					
+					<div class="text">
+		
+						<p class="headline">Red with Envy</p>
+			
+						<p class="tag">Hunter Hayes’ first european release drops in the UKon april 25th. Check out his new look.</p>
+		
+						<a class="btn">Check it out</a>
+
+					</div><!--text-->
+		
+				</div><!--inner-->
+
 			    <img src="/_assets/img/slider-hayes.jpg" alt="Hunter Hayes cd packaging design" title="Hunter Hayes, I Want Crazy album design" />
+		
 			</li>
+		
 		</ul>
 
-	</div>
+	</div><!--flexslider-->
 
 </section><!--slider-->
+
+<section id="about">
+	
+	<h3>Who is Stunt Double Creative?</h3>
+
+</section><!--about-->
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
