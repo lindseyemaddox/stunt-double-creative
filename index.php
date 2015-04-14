a0e36548d5b3ef44a314d9f3f1608919d179f1a9
lindseyemaddox <lindseyemaddox@gmail.com>
2015-04-14 00:10:43 -0500

    start on services, add icon font

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 		
 						<p class="headline">Red with Envy</p>
 			
-						<p class="tag">Hunter Hayes’ first european release drops in the UKon april 25th. Check out his new look.</p>
+						<p class="tag">Hunter Hayes’ first european release drops in the UK on april 25th. Check out his new look.</p>
 		
-						<div class="btnBox"><a class="btn alt" href="#portfolio">Check it out</a></div>
+						<div class="btnBox"><a class="btn alt fancybox" href="#branding-item">Check it out</a></div>
 
 					</div><!--text-->
 		
@@ -162,11 +162,145 @@
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis mi eu elit tempor facilisis id et neque. Nulla sit amet sem sapien. Vestibulum imperdiet porta ante ac ornare. Nulla et lorem eu nibh adipiscing ultricies nec at lacus. Cras laoreet ultricies sem, at blandit mi eleifend aliquam. Nunc enim ipsum, vehicula non pretium varius, cursus ac tortor. Vivamus fringilla congue laoreet. Quisque ultrices sodales orci, quis rhoncus justo auctor in. Phasellus dui eros, bibendum eu feugiat ornare, faucibus eu mi. Nunc aliquet tempus sem, id aliquam diam varius ac. Maecenas nisl nunc, molestie vitae eleifend vel, iaculis sed magna. Aenean tempus lacus vitae orci posuere porttitor eget non felis. Donec lectus elit, aliquam nec eleifend sit amet, vestibulum sed nunc.</p>
 			</div>
 
+			<a class="portfolio-item fancybox branding" href="#branding-item" title="Branding Item" data-fancybox-group="branding">
+
+				<div class="overlay"></div><!--overlay-->
+
+				<div class="text">
+
+					<p class="project-title">Project Title</p>
+
+					<p class="project-details">Project info can run onto multiple lines if needed.</p>
+
+				</div><!--text-->
+
+				<img src="/_assets/img/portfolio-placeholder.jpg" alt="" title="">
+
+			</a><!--portfolio-item-->
+		
+			<div id="branding-item" style="width:400px;display: none;">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis mi eu elit tempor facilisis id et neque. Nulla sit amet sem sapien. Vestibulum imperdiet porta ante ac ornare. Nulla et lorem eu nibh adipiscing ultricies nec at lacus. Cras laoreet ultricies sem, at blandit mi eleifend aliquam. Nunc enim ipsum, vehicula non pretium varius, cursus ac tortor. Vivamus fringilla congue laoreet. Quisque ultrices sodales orci, quis rhoncus justo auctor in. Phasellus dui eros, bibendum eu feugiat ornare, faucibus eu mi. Nunc aliquet tempus sem, id aliquam diam varius ac. Maecenas nisl nunc, molestie vitae eleifend vel, iaculis sed magna. Aenean tempus lacus vitae orci posuere porttitor eget non felis. Donec lectus elit, aliquam nec eleifend sit amet, vestibulum sed nunc.</p>
+			</div>
+
+			<a class="portfolio-item fancybox branding" href="#branding-item2" title="Branding Item 2" data-fancybox-group="branding">
+
+				<div class="overlay"></div><!--overlay-->
+
+				<div class="text">
+
+					<p class="project-title">Project Title</p>
+
+					<p class="project-details">Project info can run onto multiple lines if needed.</p>
+
+				</div><!--text-->
+
+				<img src="/_assets/img/portfolio-placeholder.jpg" alt="" title="">
+
+			</a><!--portfolio-item-->
+		
+			<div id="branding-item2" style="width:400px;display: none;">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis mi eu elit tempor facilisis id et neque. Nulla sit amet sem sapien. Vestibulum imperdiet porta ante ac ornare. Nulla et lorem eu nibh adipiscing ultricies nec at lacus. Cras laoreet ultricies sem, at blandit mi eleifend aliquam. Nunc enim ipsum, vehicula non pretium varius, cursus ac tortor. Vivamus fringilla congue laoreet. Quisque ultrices sodales orci, quis rhoncus justo auctor in. Phasellus dui eros, bibendum eu feugiat ornare, faucibus eu mi. Nunc aliquet tempus sem, id aliquam diam varius ac. Maecenas nisl nunc, molestie vitae eleifend vel, iaculis sed magna. Aenean tempus lacus vitae orci posuere porttitor eget non felis. Donec lectus elit, aliquam nec eleifend sit amet, vestibulum sed nunc.</p>
+			</div>
+
+			<a class="portfolio-item fancybox print" href="#print-item" title="Print Item" data-fancybox-group="print">
+
+				<div class="overlay"></div><!--overlay-->
+
+				<div class="text">
+
+					<p class="project-title">Project Title</p>
+
+					<p class="project-details">Project info can run onto multiple lines if needed.</p>
+
+				</div><!--text-->
+
+				<img src="/_assets/img/portfolio-placeholder.jpg" alt="" title="">
+
+			</a><!--portfolio-item-->
+		
+			<div id="print-item" style="width:400px;display: none;">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis mi eu elit tempor facilisis id et neque. Nulla sit amet sem sapien. Vestibulum imperdiet porta ante ac ornare. Nulla et lorem eu nibh adipiscing ultricies nec at lacus. Cras laoreet ultricies sem, at blandit mi eleifend aliquam. Nunc enim ipsum, vehicula non pretium varius, cursus ac tortor. Vivamus fringilla congue laoreet. Quisque ultrices sodales orci, quis rhoncus justo auctor in. Phasellus dui eros, bibendum eu feugiat ornare, faucibus eu mi. Nunc aliquet tempus sem, id aliquam diam varius ac. Maecenas nisl nunc, molestie vitae eleifend vel, iaculis sed magna. Aenean tempus lacus vitae orci posuere porttitor eget non felis. Donec lectus elit, aliquam nec eleifend sit amet, vestibulum sed nunc.</p>
+			</div>
+
+			<a class="portfolio-item fancybox web-design" href="#web-design-item" title="Web Design Item" data-fancybox-group="web-design">
+
+				<div class="overlay"></div><!--overlay-->
+
+				<div class="text">
+
+					<p class="project-title">Project Title</p>
+
+					<p class="project-details">Project info can run onto multiple lines if needed.</p>
+
+				</div><!--text-->
+
+				<img src="/_assets/img/portfolio-placeholder.jpg" alt="" title="">
+
+			</a><!--portfolio-item-->
+		
+			<div id="web-design-item" style="width:400px;display: none;">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis mi eu elit tempor facilisis id et neque. Nulla sit amet sem sapien. Vestibulum imperdiet porta ante ac ornare. Nulla et lorem eu nibh adipiscing ultricies nec at lacus. Cras laoreet ultricies sem, at blandit mi eleifend aliquam. Nunc enim ipsum, vehicula non pretium varius, cursus ac tortor. Vivamus fringilla congue laoreet. Quisque ultrices sodales orci, quis rhoncus justo auctor in. Phasellus dui eros, bibendum eu feugiat ornare, faucibus eu mi. Nunc aliquet tempus sem, id aliquam diam varius ac. Maecenas nisl nunc, molestie vitae eleifend vel, iaculis sed magna. Aenean tempus lacus vitae orci posuere porttitor eget non felis. Donec lectus elit, aliquam nec eleifend sit amet, vestibulum sed nunc.</p>
+			</div>
+
 		</div><!--container-->
 
   	</div><!--inner-->
 
-</section><!--testimonials-->
+</section><!--portfolio-->
+
+<section id="team">
+	
+	<div class="inner">
+		
+		<h3>Our Team.</h3>
+
+		<h4>So you can find us at the coffee shop.</h4>
+
+		<div class="left">
+			
+			<img src="/_assets/img/alex-fortney.jpg" alt="Alex Fortney, Creative Director at Stunt Double Creative" title="Alex Fortney, Creative Director at Stunt Double Creative">
+
+			<h5>Alex Fortney</h5>
+
+			<h4>Creative Director</h4>
+
+			<p>Alex loves making things, but her true passions are all things that could kill her, like playing rugby for the Nashville women’s team, riding vintage motorcycles, and eating Thai food after 11pm.</p>
+
+		</div><!--left-->
+
+		<div class="right">
+			
+			<img src="/_assets/img/alison-slamon.jpg" alt="Alison Slamon, Chief of Stunts at Stunt Double Creative" title="Alison Slamon, Chief of Stunts at Stunt Double Creative">
+
+			<h5>Alison Slamon</h5>
+
+			<h4>Chief of Stunts</h4>
+
+			<p>Born in Las Vegas and raised in San Diego, Alison fled the wild west over a decade ago for Music City. After cutting her teeth in advertising, she is now the wearer of many hats at Stunt Double Creative.</p>
+
+		</div><!--right-->
+
+	</div><!--inner-->
+
+</section><!--team-->
+
+<section id="services">
+	
+	<div class="inner">
+		
+		<h3>Services.</h3>
+
+		<h4>This is what we’re good at.</h4>
+
+		<div id="service">
+			
+			
+			
+		</div><!--service-->
+
+	</div><!--inner-->
+
+</section><!--services-->
 
 
 <script>
